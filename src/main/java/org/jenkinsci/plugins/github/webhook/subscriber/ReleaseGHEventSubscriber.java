1e3a020ea1e283922429beefc7fea0442b848b2a
Olivier Schiavo <olivier.schiavo@wengo.com>
2020-04-28 14:25:27 +0200

    * (wip) release trigger for github events

---
+++ src/main/java/org/jenkinsci/plugins/github/webhook/subscriber/ReleaseGHEventSubscriber.java
@@ -0,0 +1,134 @@
+package org.jenkinsci.plugins.github.webhook.subscriber;
+
+import com.cloudbees.jenkins.GitHubPushTrigger;
+import com.cloudbees.jenkins.GitHubRepositoryName;
+import com.cloudbees.jenkins.GitHubRepositoryNameContributor;
+import com.cloudbees.jenkins.GitHubTriggerEvent;
+import com.cloudbees.jenkins.GitHubWebHook;
+import hudson.Extension;
+import hudson.ExtensionList;
+import hudson.model.Item;
+import hudson.security.ACL;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.github.extension.GHSubscriberEvent;
+import org.jenkinsci.plugins.github.extension.GHEventsSubscriber;
+import org.kohsuke.github.GHEvent;
+import org.kohsuke.github.GHEventPayload;
+import org.kohsuke.github.GitHub;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+import static com.google.common.collect.Sets.immutableEnumSet;
+import static org.jenkinsci.plugins.github.util.JobInfoHelpers.triggerFrom;
+import static org.jenkinsci.plugins.github.util.JobInfoHelpers.withTrigger;
+import static org.kohsuke.github.GHEvent.PUSH;
+
+/**
+ * By default this plugin interested in push events only when job uses {@link GitHubPushTrigger}
+ *
+ * @author lanwen (Merkushev Kirill)
+ * @since 1.12.0
+ */
+@Extension
+@SuppressWarnings("unused")
+public class ReleaseGHEventSubscriber extends GHEventsSubscriber {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReleaseGHEventSubscriber.class);
+
+    /**
+     * This subscriber is applicable only for job with GHPush trigger
+     *
+     * @param project to check for trigger
+     *
+     * @return true if project has {@link GitHubPushTrigger}
+     */
+    @Override
+    protected boolean isApplicable(Item project) {
+        return withTrigger(GitHubPushTrigger.class).apply(project);
+    }
+
+    /**
+     * @return set with only push event
+     */
+    @Override
+    protected Set<GHEvent> events() {
+        return immutableEnumSet(RELEASE);
+    }
+
+    /**
+     * Calls {@link GitHubPushTrigger} in all projects to handle this hook
+     *
+     * @param event   only RELEASE event
+     */
+    @Override
+    protected void onEvent(final GHSubscriberEvent event) {
+        GHEventPayload.Release release;
+        try {
+            release = GitHub.offline().parseEventPayload(new StringReader(event.getPayload()), GHEventPayload.Release.class);
+        } catch (IOException e) {
+            LOGGER.warn("Received malformed ReleaseEvent: " + event.getPayload(), e);
+            return;
+        }
+        URL repoUrl = release.getRepository().getUrl();
+        final String releaseName = release.getRelease().getName();
+        LOGGER.info("Received ReleaseEvent for {} from {}", repoUrl, event.getOrigin());
+        GitHubRepositoryName fromEventRepository = GitHubRepositoryName.create(repoUrl.toExternalForm());
+
+        if (fromEventRepository == null) {
+            // On push event on github.com url === html_url
+            // this is not consistent with the API docs and with hosted repositories
+            // see https://goo.gl/c1qmY7
+            // let's retry with 'html_url'
+            URL htmlUrl = release.getRepository().getHtmlUrl();
+            fromEventRepository = GitHubRepositoryName.create(htmlUrl.toExternalForm());
+            if (fromEventRepository != null) {
+                LOGGER.debug("ReleaseEvent handling: 'html_url' field "
+                        + "has been used to retrieve project information (instead of default 'url' field)");
+            }
+        }
+
+        final GitHubRepositoryName changedRepository = fromEventRepository;
+
+        if (changedRepository != null) {
+            // run in high privilege to see all the projects anonymous users don't see.
+            // this is safe because when we actually schedule a build, it's a build that can
+            // happen at some random time anyway.
+            ACL.impersonate(ACL.SYSTEM, new Runnable() {
+                @Override
+                public void run() {
+                    for (Item job : Jenkins.getInstance().getAllItems(Item.class)) {
+                        GitHubPushTrigger trigger = triggerFrom(job, GitHubPushTrigger.class);
+                        if (trigger != null) {
+                            String fullDisplayName = job.getFullDisplayName();
+                            LOGGER.debug("Considering to poke {}", fullDisplayName);
+                            if (GitHubRepositoryNameContributor.parseAssociatedNames(job)
+                                    .contains(changedRepository)) {
+                                LOGGER.info("Poked {}", fullDisplayName);
+                                trigger.onPost(GitHubTriggerEvent.create()
+                                        .withTimestamp(event.getTimestamp())
+                                        .withOrigin(event.getOrigin())
+                                        .withTriggeredByUser(pusherName)
+                                        .build()
+                                );
+                            } else {
+                                LOGGER.debug("Skipped {} because it doesn't have a matching repository.",
+                                        fullDisplayName);
+                            }
+                        }
+                    }
+                }
+            });
+
+            for (GitHubWebHook.Listener listener : ExtensionList.lookup(GitHubWebHook.Listener.class)) {
+                listener.onPushRepositoryChanged(pusherName, changedRepository);
+            }
+
+        } else {
+            LOGGER.warn("Malformed repo url {}", repoUrl);
+        }
+    }
+}
